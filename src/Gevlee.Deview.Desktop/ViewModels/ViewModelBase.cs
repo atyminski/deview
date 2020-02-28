ee17f8f6776e2e9d394083e4ecc9e22abc4eb366
Arkadiusz Tymiński <tyminski.arek@hotmail.com>
2020-02-28 18:38:58 +0100

    Initialize

---
+++ src/Gevlee.Deview.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Gevlee.Deview.Desktop.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ src/Gevlee.Deview.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using AvalonStudio.Shell.Controls;
+
+namespace Gevlee.Deview.Desktop.Views
+{
+    public class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
